d149b005f5592f57c2a1b469bbe6cfeb41643a8d
guokairui <wxsms@foxmail.com>
2023-03-15 11:44:40 +0800

    watch

--- reactivity/5-watch.js
+++ reactivity/5-watch.js
@@ -1,3 +1,18 @@
 import { effect } from './1-effect';
 
 export const watchEffect = effect;
+
+export function watch (source, callback) {
+  let oldValue;
+  let firstrun = true;
+
+  return effect(() => {
+    if (firstrun) {
+      firstrun = false;
+      oldValue = JSON.parse(JSON.stringify(source));
+      return;
+    }
+    callback(source, oldValue);
+    oldValue = JSON.parse(JSON.stringify(source));
+  });
+}
--- reactivity/5-watch.spec.js
+++ reactivity/5-watch.spec.js
@@ -1,5 +1,6 @@
 import { ref } from './3-ref';
-import { watchEffect } from './5-watch';
+import { watch, watchEffect } from './5-watch';
+import { reactive } from './2-reactive';
 
 const { jest } = import.meta;
 
@@ -22,5 +23,26 @@ describe('watch', () => {
 
     a.value = 600;
     expect(b.value).toEqual(600);
+  });
+
+  it('watch reactive', () => {
+    let a = reactive({ value: 1 });
+    let b = 0;
+
+    let stop = watch(a, (val, oldVal) => {
+      b = oldVal.value + val.value;
+    });
+    expect(b).toEqual(0);
+
+    a.value = 100;
+    expect(b).toEqual(101);
+
+    a.value = 200;
+    expect(b).toEqual(300);
+
+    stop();
+
+    a.value = 500;
+    expect(b).toEqual(300);
   });
 });
